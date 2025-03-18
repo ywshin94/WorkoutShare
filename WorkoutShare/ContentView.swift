857f7ac421589cc2572f84e8855315fb613be8f3
ywshin94 <ywshin94@gmail.com>
2025-03-19 06:36:38 +0900

    Initial Commit

---
+++ WorkoutShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutShare
+//
+//  Created by 신용운 on 3/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutShare/WorkoutShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutShareApp.swift
+//  WorkoutShare
+//
+//  Created by 신용운 on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
